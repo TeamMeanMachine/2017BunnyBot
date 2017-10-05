f6914441f8c3f37b3949be1d907f812f07bb26a0
lukeg32 <lukegantar@gmail.com>
2017-10-04 19:56:15 -0700

    added the ablility to drive with joystick in the robot

--- src/main/kotlin/org/team2471/bunnybots/robot/Robot.kt
+++ src/main/kotlin/org/team2471/bunnybots/robot/Robot.kt
@@ -1,10 +1,18 @@
 package org.team2471.bunnybots.robot
 
 
+import edu.wpi.first.wpilibj.GenericHID
 import edu.wpi.first.wpilibj.IterativeRobot
+import edu.wpi.first.wpilibj.XboxController
+import org.team2471.bunnybots.robot.subsystems.Drive
 
 class Robot : IterativeRobot() {
-    override fun autonomousInit() {
-
+    private val controller: XboxController = XboxController(0)
+    override fun robotInit() {
+        Drive
+    }
+    override fun teleopPeriodic() {
+        Drive.drive(controller.getY(GenericHID.Hand.kLeft),controller.getX(GenericHID.Hand.kRight),
+                controller.getTriggerAxis(GenericHID.Hand.kRight)-controller.getTriggerAxis(GenericHID.Hand.kLeft))
     }
 }
---
+++ src/main/kotlin/org/team2471/bunnybots/robot/subsystems/Drive.kt
@@ -0,0 +1,31 @@
+package org.team2471.bunnybots.robot.subsystems
+
+
+import com.ctre.MotorControl.CANTalon
+import edu.wpi.first.wpilibj.Solenoid
+
+object Drive {
+    private val leftMotor1 = CANTalon(14)
+    private val leftMotor2 = CANTalon(13)
+    private val rightMotor1 = CANTalon(1)
+    private val rightMotor2 = CANTalon(0)
+
+    fun drive(throttle: Double, softTurn: Double, hardTurn: Double) {
+        var leftPower = throttle + (softTurn * Math.abs(throttle))
+        var rightPower = throttle - (softTurn * Math.abs(throttle))
+        leftPower += hardTurn
+        rightPower -= hardTurn
+
+        val maxPower = Math.max(Math.abs(leftPower), Math.abs(rightPower))
+        if (maxPower > 1){
+            leftPower /= maxPower
+            rightPower /= maxPower
+        }
+
+        leftMotor1.set(leftPower)
+        rightMotor1.set(-rightPower)
+        // TODO: make motors 2 and 3 followers
+        leftMotor2.set(leftPower)
+        rightMotor2.set(-rightPower)
+    }
+}
