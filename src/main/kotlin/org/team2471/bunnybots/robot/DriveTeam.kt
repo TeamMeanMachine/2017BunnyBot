3a2448e3dc8bec593a218dbc7216083446a36a1d
tylerscheuble <tylerscheuble@gmail.com>
2017-10-23 19:52:51 -0700

    Update base files

---
+++ src/main/kotlin/org/team2471/bunnybots/robot/DriveTeam.kt
@@ -0,0 +1,11 @@
+package org.team2471.bunnybots.robot
+
+import edu.wpi.first.wpilibj.XboxController
+
+object Driver {
+    private val controller = XboxController(0)
+}
+
+object CoDriver {
+    private val controller = XboxController(1)
+}
--- src/main/kotlin/org/team2471/bunnybots/robot/Robot.kt
+++ src/main/kotlin/org/team2471/bunnybots/robot/Robot.kt
@@ -1,18 +1,26 @@
 package org.team2471.bunnybots.robot
 
 
-import edu.wpi.first.wpilibj.GenericHID
 import edu.wpi.first.wpilibj.IterativeRobot
-import edu.wpi.first.wpilibj.XboxController
-import org.team2471.bunnybots.robot.subsystems.Drive
+import org.team2471.frc.lib.control.command.Scheduler
 
 class Robot : IterativeRobot() {
-    private val controller: XboxController = XboxController(0)
     override fun robotInit() {
-        Drive
     }
+
+    override fun robotPeriodic() {
+        Scheduler.tick()
+    }
+
+    override fun autonomousInit() {
+    }
+
+    override fun autonomousPeriodic() {
+    }
+
+    override fun teleopInit() {
+    }
+
     override fun teleopPeriodic() {
-        Drive.drive(controller.getY(GenericHID.Hand.kLeft),controller.getX(GenericHID.Hand.kRight),
-                controller.getTriggerAxis(GenericHID.Hand.kRight)-controller.getTriggerAxis(GenericHID.Hand.kLeft))
     }
 }
