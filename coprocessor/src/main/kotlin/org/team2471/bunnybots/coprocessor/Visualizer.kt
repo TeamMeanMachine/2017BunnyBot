a8fc229ea1cb398b2c6c652e7c6b81e55b2b3061
Justine P <justinetulip@gmail.com>
2017-10-30 21:03:51 -0700

    Mostly got some lidar stuffs + visualization

--- coprocessor/src/main/kotlin/org/team2471/bunnybots/coprocessor/Visualizer.kt
+++ coprocessor/src/main/kotlin/org/team2471/bunnybots/coprocessor/Visualizer.kt
@@ -1,20 +1,26 @@
 package org.team2471.bunnybots.coprocessor
 
-import javafx.scene.chart.XYChart
+import org.knowm.xchart.SwingWrapper
 import org.knowm.xchart.XYChartBuilder
 import org.knowm.xchart.XYSeries
 import org.knowm.xchart.style.Styler
-import org.team2471.frc.lib.math.Point2D
+import org.team2471.frc.lib.math.Point
 
-fun visualize(clusterMap: Map<Int, List<Point2D>>){
+fun visualize(clusterMap: Map<Int, List<Point>>){
 
-    val chart = XYChartBuilder().width(600).height(500)
+    val chart = XYChartBuilder().width(1080).height(1000)
             .title("Blobs").xAxisTitle("X").yAxisTitle("Y").build()
 
     chart.styler.defaultSeriesRenderStyle = XYSeries.XYSeriesRenderStyle.Scatter
     chart.styler.isChartTitleVisible = false
     chart.styler.legendPosition = Styler.LegendPosition.InsideSW
-    chart.styler.markerSize = 16
+    chart.styler.markerSize = 6
+
+    clusterMap.forEach {cluster, points ->
+        val label = if (cluster == -1) "Noise" else "Cluster $cluster"
+        chart.addSeries(label, points.map { it.x }.toDoubleArray(), points.map { it.y }.toDoubleArray())
+    }
+    SwingWrapper(chart).displayChart()
 
 }
 
